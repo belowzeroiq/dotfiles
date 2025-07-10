ce16d548ec50e8ebcb2c0ab62aa5922abaa6eda9
env. <162432464+belowzeroiq@users.noreply.github.com>
2025-07-10 20:54:15 +0700

    fixup powermenu functionality

--- waybar/scripts/rofi-power-menu.sh
+++ waybar/scripts/rofi-power-menu.sh
@@ -3,14 +3,33 @@
 # Options
 options="🔒 Lock\n🚪 Logout\n💤 Suspend\n🔁 Reboot\n⏻ Shutdown"
 
-# Run rofi in custom mode (not dmenu)
-chosen=$(echo -e "$options" | rofi -show power -modi "power:echo -e '$options'" -theme synth-clean)
+# Run rofi with better spacing - using window mode instead of dmenu
+chosen=$(echo -e "$options" | rofi -dmenu -p "Power Menu" -theme synth-clean \
+    -lines 5 \
+    -columns 1 \
+    -width 25 \
+    -location 0 \
+    -yoffset 0 \
+    -xoffset 0 \
+    -fixed-num-lines \
+    -no-custom \
+    -font "mono 12")
 
 # Act on choice
 case "$chosen" in
-    "🔒 Lock") hyprctl dispatch dpms off && swaylock ;;
-    "🚪 Logout") hyprctl dispatch exit ;;
-    "💤 Suspend") systemctl suspend ;;
-    "🔁 Reboot") systemctl reboot ;;
-    "⏻ Shutdown") systemctl poweroff ;;
+    "🔒 Lock")
+        hyprctl dispatch dpms off && hyprlock
+        ;;
+    "🚪 Logout")
+        hyprctl dispatch exit
+        ;;
+    "💤 Suspend")
+        hyprlock && systemctl suspend
+        ;;
+    "🔁 Reboot")
+        systemctl reboot
+        ;;
+    "⏻ Shutdown")
+        systemctl poweroff
+        ;;
 esac
